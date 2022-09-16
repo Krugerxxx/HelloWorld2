0ec40acade88b18cf47cd20574081c9ca502470d
Krugerxxx <sssnik@mail.ru>
2022-09-16 13:12:55 +0500

    v1.0.2

---
+++ src/java/ru/evdokimov/Example.java
@@ -0,0 +1,11 @@
+package java.ru.evdokimov;
+
+public class Example {
+    private String example;
+
+    public Example(String example) {
+        this.example = example;
+        System.out.println(this.example);
+    }
+
+}
